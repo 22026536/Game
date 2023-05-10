cb1b6059c728c3423735f3717f7eb3b602f79f9a
22026536 <125473184+22026536@users.noreply.github.com>
2023-05-10 22:14:46 +0700

    Add files via upload

---
+++ Game/flyingcatlib.cpp
@@ -0,0 +1,9 @@
+#include"flyingcatlib.h"
+const int SCREEN_WIDTH = 1200;
+const int SCREEN_HEIGHT = 679;
+
+std::string toString(int i){
+    std:: stringstream ss;
+    ss<<i;
+    return ss.str(); 
+}
---
+++ Game/flyingcatlib.h
@@ -0,0 +1,166 @@
+#include<SDL.h>
+#include<SDL_image.h>
+#include<SDL_mixer.h>
+#include<mixer.h>
+#include<SDL_ttf.h>
+#include<stdio.h>
+#include<string>
+#include<sstream>
+#include<iostream>
+#include<cstdlib>
+#include<time.h>
+
+//class ảnh
+class LTexture{
+    public:
+        LTexture();
+
+        ~LTexture();
+
+        void free();
+
+        //load file ảnh
+        bool loadFromFile( std::string path, SDL_Renderer *gRenderer);
+
+        //tạo ảnh text
+        bool loadFromRenderedText( std::string textureText, SDL_Color textColor, SDL_Renderer *gRenderer, TTF_Font *gFont);
+
+        //thay đổi màu của ảnh
+        void setColor();
+
+        //Set alpha ảnh
+        void setAlpha( Uint8 alpha );
+
+        //render ảnh
+        void render(int x, int y, SDL_Renderer *gRenderer, double angle = 0.0 ,SDL_Rect* clip = NULL,  SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE);
+
+        //check xem con chuột có ở trong ảnh không
+        bool checkMouse();
+
+        int mWidth;
+        int mHeight;
+        int mPosX;
+        int mPosY;
+
+    private:
+        SDL_Texture* mTexture;
+};
+
+//class con mèo
+class LCat{
+    public:
+        static const int CAT_IMAGE_WIDTH = 222;
+        static const int CAT_WIDTH = 100 ;
+        static const int CAT_HEIGHT = 80 ;
+
+        int lineToRender;
+        int catMotion;
+        int mPosX;
+        int mPosY;
+
+        LCat();
+
+        //If a key was pressed
+        void handleEvent (SDL_Event& e);
+
+        //is cat moving
+        bool isMove();
+
+        //check distance
+        double distance(double x1,double y1,double x2, double y2);
+        
+        //check is eat food
+        bool check(int mFoodX, int mFoodWidth, int mFoodLine);
+
+        //render cat image
+        void render(SDL_Renderer *gRenderer);
+
+        //render cat image motion
+        void renderMotion();
+    
+    private:
+    int mPosY_;
+    const int mVelY=12;
+
+};
+
+//class đồ ăn
+class LFood{
+    public:
+        const int FOOD_WIDTH=85;
+        const int FOOD_HEIGHT=80;
+        int mPosX;
+        int mPosY;
+        int mVelX=7;
+        int foodToRender;
+        int lineToRender;
+
+        LFood();
+
+        //render food image
+        void render();
+
+        //render food image motion
+        void renderMotion();
+
+    private:
+};
+
+//class cầu vòng sau con mèo
+class LRainbow{
+    public:
+        int mPosX;
+        int mPosY;
+        int rainbowMotion;
+        const int RAINBOW_WIDTH = 30;   
+        double mVelY;
+    
+    LRainbow();
+    
+    //render rainbow image
+    void render();
+
+    //render rainbow image motion
+    void renderMotion();
+
+    private:
+};
+
+//hàm khởi tạo
+bool init();
+
+//hàm load ảnh
+bool loadMedia();
+
+//hàm đóng
+void close();
+
+//set tốc độ của từng cầu vồng
+void setRainbow();
+
+//set các thông số trước khi về menu
+void toMenu();
+
+//set các thông số trước khi restart
+void toRestart();
+
+//hàm chạy game khi chưa bắt đầu
+void whileHaveNotStarted();
+
+//hàm chạy khi dừng game
+void whilePauseGame();
+
+//hàm chạy khi chơi game
+void whilePlayGame();
+
+//hàm kiểm tra xem có được điểm cao nhất và cài đặt chữ khi kết thúc
+void checkScore();
+
+//hàm chạy khi kết thúc game
+void whileEndGame(int &alpha);
+
+//quit game
+void toQuit();
+
+//chuyen int ve string
+std::string toString(int number);
