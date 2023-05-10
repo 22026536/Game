4bf6d07bf7d17bd17c372c4ef3a0f127550fad8b
22026536 <125473184+22026536@users.noreply.github.com>
2023-05-10 22:00:56 +0700

    Delete fileCode directory

--- fileCode/flyingcatlib.cpp
+++
@@ -1,9 +0,0 @@
-#include"flyingcatlib.h"
-const int SCREEN_WIDTH = 1200;
-const int SCREEN_HEIGHT = 679;
-
-std::string toString(int i){
-    std:: stringstream ss;
-    ss<<i;
-    return ss.str(); 
-}
